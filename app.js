395a018b7ea3445034f5311e411b76be9305c658
Zhi Liang <zkl119@ic.ac.uk>
2021-12-18 12:56:34 +0000

    Added prints

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ app.use(express.json());
 
 app.get('/moreQuestions', async (req, res) => {
   const { number } = req.body;
+  console.log(number)
   axios.get(`https://opentdb.com/api.php?amount=${number}&type=multiple`)
     .then((resp) => {
+      print(resp.data)
       const results = resp.data.results
       res.status(200).send(results)
     });
